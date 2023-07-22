f75528524478ef2aae0694662dff8d167ee7e41f
hypoxisaurea <flickerlight14@naver.com>
2023-07-22 23:32:48 +0900

    NLP study

---
+++ word2vec.py
@@ -0,0 +1,113 @@
+from collections import Counter
+from typing import re
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from tensorflow.python.keras.models import load_model
+
+from tqdm import tqdm
+from konlpy.tag import Okt
+from keras.src.utils import pad_sequences
+from keras.src.preprocessing.text import Tokenizer
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Embedding, LSTM, Dense
+from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
+
+#----------- 데이터 삽입 --------------
+train = pd.read_table("ratings_train.txt")
+test = pd.read_table("ratings_test.txt")
+
+print('학습 데이터 전체 개수: {}'.format(len(train)))
+print('시험 데이터 전체 개수: {}'.format(len(test)))
+
+#----------- 전처리 --------------
+train.drop_duplicates(subset=['document'], inplace=True) #중복제거
+test.drop_duplicates(subset=['document'], inplace=True) #중복제거
+
+train['document'] = train['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+train['document'] = train['document'].str.replace('^ +', "")
+train['document'].replace('', np.nan, inplace=True)
+
+test['document'] = test['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+test['document'] = test['document'].str.replace('^ +', "")
+test['document'].replace('', np.nan, inplace=True)
+
+train = train.dropna(how = 'any') #Null값 제거
+test = test.dropna(how = 'any') #Null값 제거
+
+print('\n전처리 후 학습 데이터 전체 개수: {}'.format(len(train)))
+print('전처리 후 시험 데이터 전체 개수: {}'.format(len(test)))
+
+#----------- 토큰화 --------------
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+okt = Okt()
+
+X_train = []
+for sentence in tqdm(train['document']):
+    tokenized_sentence = okt.morphs(sentence, stem=True)
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords]
+    X_train.append(stopwords_removed_sentence)
+
+X_test = []
+for sentence in tqdm(test['document']):
+    tokenized_sentence = okt.morphs(sentence, stem=True)
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords]
+    X_test.append(stopwords_removed_sentence)
+
+#----------- 정수 인코딩 --------------
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+threshold = 3
+total_cnt = len(tokenizer.word_index)
+rare_cnt = 0
+total_freq = 0
+rare_freq = 0
+
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+print('\n단어 집합(vocabulary)의 크기 :',total_cnt)
+print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
+
+vocab_size = total_cnt - rare_cnt + 1 # 전체 단어 개수 중 빈도수 2이하인 단어는 제거, 0번 패딩 토큰을 고려하여 + 1
+
+tokenizer = Tokenizer(vocab_size)
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+y_train = np.array(train['label'])
+y_test = np.array(test['label'])
+
+#----------- 패딩 --------------
+print('리뷰의 최대 길이 :',max(len(review) for review in X_train))
+print('리뷰의 평균 길이 :',sum(map(len, X_train))/len(X_train))
+
+def below_threshold_len(max_len, nested_list):
+  count = 0
+  for sentence in nested_list:
+    if(len(sentence) <= max_len):
+        count = count + 1
+  print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (count / len(nested_list))*100))
+
+max_len = 30
+below_threshold_len(max_len, X_train)
+
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+#----------- 빈 샘플 제거 --------------
+drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+X_train = np.delete(X_train, drop_train, axis=0)
+y_train = np.delete(y_train, drop_train, axis=0)
+
+print('\n빈 샘플 제거 후 데이터 전체 개수: {}'.format(len(X_train)))
+print('빈 샘플 제거 후 데이터 전체 개수: {}'.format(len(y_train)))
