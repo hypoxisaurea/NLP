a3d0085eaeca0d1b7054fc3a681193f642c1f5ca
hypoxisaurea <flickerlight14@naver.com>
2023-07-25 08:48:08 +0900

    attention study

---
+++ text_summerization_attention.py
@@ -0,0 +1,380 @@
+import re
+import nltk
+import urllib
+import numpy as np
+import pandas as pd
+
+from bs4 import BeautifulSoup
+from matplotlib import pyplot as plt
+from nltk.corpus import stopwords
+from tensorflow.python.keras.callbacks import EarlyStopping
+
+from keras.utils import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from tensorflow.python.keras import Input, Model
+from tensorflow.python.keras.layers import LSTM, Embedding, Dense, Concatenate
+
+#----------- 데이터 삽입 --------------
+np.random.seed(seed=0)
+data = pd.read_csv("content/Reviews.csv", nrows = 100000)
+data = data[['Text','Summary']]
+data.drop_duplicates(subset=['Text'], inplace=True)
+data.dropna(axis=0, inplace=True)
+print('전처리 전 전체 샘플수 :',(len(data)))
+
+#----------- 전처리 --------------
+contractions = {"'cause": 'because',
+ "I'd": 'I would',
+ "I'd've": 'I would have',
+ "I'll": 'I will',
+ "I'll've": 'I will have',
+ "I'm": 'I am',
+ "I've": 'I have',
+ "ain't": 'is not',
+ "aren't": 'are not',
+ "can't": 'cannot',
+ "could've": 'could have',
+ "couldn't": 'could not',
+ "didn't": 'did not',
+ "doesn't": 'does not',
+ "don't": 'do not',
+ "hadn't": 'had not',
+ "hasn't": 'has not',
+ "haven't": 'have not',
+ "he'd": 'he would',
+ "he'll": 'he will',
+ "he's": 'he is',
+ "here's": 'here is',
+ "how'd": 'how did',
+ "how'd'y": 'how do you',
+ "how'll": 'how will',
+ "how's": 'how is',
+ "i'd": 'i would',
+ "i'd've": 'i would have',
+ "i'll": 'i will',
+ "i'll've": 'i will have',
+ "i'm": 'i am',
+ "i've": 'i have',
+ "isn't": 'is not',
+ "it'd": 'it would',
+ "it'd've": 'it would have',
+ "it'll": 'it will',
+ "it'll've": 'it will have',
+ "it's": 'it is',
+ "let's": 'let us',
+ "ma'am": 'madam',
+ "mayn't": 'may not',
+ "might've": 'might have',
+ "mightn't": 'might not',
+ "mightn't've": 'might not have',
+ "must've": 'must have',
+ "mustn't": 'must not',
+ "mustn't've": 'must not have',
+ "needn't": 'need not',
+ "needn't've": 'need not have',
+ "o'clock": 'of the clock',
+ "oughtn't": 'ought not',
+ "oughtn't've": 'ought not have',
+ "sha'n't": 'shall not',
+ "shan't": 'shall not',
+ "shan't've": 'shall not have',
+ "she'd": 'she would',
+ "she'd've": 'she would have',
+ "she'll": 'she will',
+ "she'll've": 'she will have',
+ "she's": 'she is',
+ "should've": 'should have',
+ "shouldn't": 'should not',
+ "shouldn't've": 'should not have',
+ "so's": 'so as',
+ "so've": 'so have',
+ "that'd": 'that would',
+ "that'd've": 'that would have',
+ "that's": 'that is',
+ "there'd": 'there would',
+ "there'd've": 'there would have',
+ "there's": 'there is',
+ "they'd": 'they would',
+ "they'd've": 'they would have',
+ "they'll": 'they will',
+ "they'll've": 'they will have',
+ "they're": 'they are',
+ "they've": 'they have',
+ "this's": 'this is',
+ "to've": 'to have',
+ "wasn't": 'was not',
+ "we'd": 'we would',
+ "we'd've": 'we would have',
+ "we'll": 'we will',
+ "we'll've": 'we will have',
+ "we're": 'we are',
+ "we've": 'we have',
+ "weren't": 'were not',
+ "what'll": 'what will',
+ "what'll've": 'what will have',
+ "what're": 'what are',
+ "what's": 'what is',
+ "what've": 'what have',
+ "when's": 'when is',
+ "when've": 'when have',
+ "where'd": 'where did',
+ "where's": 'where is',
+ "where've": 'where have',
+ "who'll": 'who will',
+ "who'll've": 'who will have',
+ "who's": 'who is',
+ "who've": 'who have',
+ "why's": 'why is',
+ "why've": 'why have',
+ "will've": 'will have',
+ "won't": 'will not',
+ "won't've": 'will not have',
+ "would've": 'would have',
+ "wouldn't": 'would not',
+ "wouldn't've": 'would not have',
+ "y'all": 'you all',
+ "y'all'd": 'you all would',
+ "y'all'd've": 'you all would have',
+ "y'all're": 'you all are',
+ "y'all've": 'you all have',
+ "you'd": 'you would',
+ "you'd've": 'you would have',
+ "you'll": 'you will',
+ "you'll've": 'you will have',
+ "you're": 'you are',
+ "you've": 'you have'}
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+def preprocess_sentence(sentence, remove_stopwords = True):
+    sentence = sentence.lower() # 텍스트 소문자화
+    sentence = BeautifulSoup(sentence, "lxml").text # <br />, <a href = ...> 등의 html 태그 제거
+    sentence = re.sub(r'\([^)]*\)', '', sentence) # 괄호로 닫힌 문자열  제거 Ex) my husband (and myself) for => my husband for
+    sentence = re.sub('"','', sentence) # 쌍따옴표 " 제거
+    sentence = ' '.join([contractions[t] if t in contractions else t for t in sentence.split(" ")]) # 약어 정규화
+    sentence = re.sub(r"'s\b","",sentence) # 소유격 제거. Ex) roland's -> roland
+    sentence = re.sub("[^a-zA-Z]", " ", sentence) # 영어 외 문자(숫자, 특수문자 등) 공백으로 변환
+    sentence = re.sub('[m]{2,}', 'mm', sentence) # m이 3개 이상이면 2개로 변경. Ex) ummmmmmm yeah -> umm yeah
+
+    if remove_stopwords:
+        tokens = ' '.join(word for word in sentence.split() if not word in stop_words if len(word) > 1)
+    else:
+        tokens = ' '.join(word for word in sentence.split() if len(word) > 1)
+    return tokens
+
+clean_text = []
+for s in data['Text']:
+    clean_text.append(preprocess_sentence(s))
+
+clean_summary = []
+for s in data['Summary']:
+    clean_summary.append(preprocess_sentence(s, 0))
+
+data['Text'] = clean_text
+data['Summary'] = clean_summary
+data.replace('', np.nan, inplace=True)
+data.dropna(axis = 0, inplace = True)
+
+text_max_len = 50
+summary_max_len = 8
+data = data[data['Text'].apply(lambda x: len(x.split()) <= text_max_len)]
+data = data[data['Summary'].apply(lambda x: len(x.split()) <= summary_max_len)]
+print('전처리 후 전체 샘플수 :',(len(data)))
+
+data['decoder_input'] = data['Summary'].apply(lambda x : 'sostoken '+ x)
+data['decoder_target'] = data['Summary'].apply(lambda x : x + ' eostoken')
+
+encoder_input = np.array(data['Text'])
+decoder_input = np.array(data['decoder_input'])
+decoder_target = np.array(data['decoder_target'])
+
+indices = np.arange(encoder_input.shape[0])
+np.random.shuffle(indices)
+
+encoder_input = encoder_input[indices]
+decoder_input = decoder_input[indices]
+decoder_target = decoder_target[indices]
+
+n_of_val = int(len(encoder_input)*0.2)
+print('테스트 데이터의 수 :',n_of_val)
+
+encoder_input_train = encoder_input[:-n_of_val]
+decoder_input_train = decoder_input[:-n_of_val]
+decoder_target_train = decoder_target[:-n_of_val]
+
+encoder_input_test = encoder_input[-n_of_val:]
+decoder_input_test = decoder_input[-n_of_val:]
+decoder_target_test = decoder_target[-n_of_val:]
+
+#----------- 정수 인코딩 --------------
+src_tokenizer = Tokenizer()
+src_tokenizer.fit_on_texts(encoder_input_train)
+threshold = 7
+total_cnt = len(src_tokenizer.word_index)
+rare_cnt = 0
+total_freq = 0
+rare_freq = 0
+
+for key, value in src_tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+src_vocab = 8000
+src_tokenizer = Tokenizer(num_words = src_vocab)
+src_tokenizer.fit_on_texts(encoder_input_train)
+encoder_input_train = src_tokenizer.texts_to_sequences(encoder_input_train)
+encoder_input_test = src_tokenizer.texts_to_sequences(encoder_input_test)
+
+tar_tokenizer = Tokenizer()
+tar_tokenizer.fit_on_texts(decoder_input_train)
+threshold = 6
+total_cnt = len(tar_tokenizer.word_index)
+rare_cnt = 0
+total_freq = 0
+rare_freq = 0
+
+for key, value in tar_tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+tar_vocab = 2000
+tar_tokenizer = Tokenizer(num_words = tar_vocab)
+tar_tokenizer.fit_on_texts(decoder_input_train)
+tar_tokenizer.fit_on_texts(decoder_target_train)
+decoder_input_train = tar_tokenizer.texts_to_sequences(decoder_input_train)
+decoder_target_train = tar_tokenizer.texts_to_sequences(decoder_target_train)
+decoder_input_test = tar_tokenizer.texts_to_sequences(decoder_input_test)
+decoder_target_test = tar_tokenizer.texts_to_sequences(decoder_target_test)
+
+#----------- 패딩 --------------
+encoder_input_train = pad_sequences(encoder_input_train, maxlen = text_max_len, padding='post')
+encoder_input_test = pad_sequences(encoder_input_test, maxlen = text_max_len, padding='post')
+decoder_input_train = pad_sequences(decoder_input_train, maxlen = summary_max_len, padding='post')
+decoder_target_train = pad_sequences(decoder_target_train, maxlen = summary_max_len, padding='post')
+decoder_input_test = pad_sequences(decoder_input_test, maxlen = summary_max_len, padding='post')
+decoder_target_test = pad_sequences(decoder_target_test, maxlen = summary_max_len, padding='post')
+
+#----------- 빈 샘플 제거 --------------
+drop_train = [index for index, sentence in enumerate(decoder_input_train) if len(sentence) == 1]
+drop_test = [index for index, sentence in enumerate(decoder_input_test) if len(sentence) == 1]
+
+encoder_input_train = np.delete(encoder_input_train, drop_train, axis=0)
+decoder_input_train = np.delete(decoder_input_train, drop_train, axis=0)
+decoder_target_train = np.delete(decoder_target_train, drop_train, axis=0)
+
+encoder_input_test = np.delete(encoder_input_test, drop_test, axis=0)
+decoder_input_test = np.delete(decoder_input_test, drop_test, axis=0)
+decoder_target_test = np.delete(decoder_target_test, drop_test, axis=0)
+
+#----------- 모델링 --------------
+embedding_dim = 128
+hidden_size = 256
+encoder_inputs = Input(shape=(text_max_len,))
+enc_emb = Embedding(src_vocab, embedding_dim)(encoder_inputs)
+
+encoder_lstm1 = LSTM(hidden_size, return_sequences=True, return_state=True ,dropout = 0.4, recurrent_dropout = 0.4)
+encoder_output1, state_h1, state_c1 = encoder_lstm1(enc_emb)
+encoder_lstm2 = LSTM(hidden_size, return_sequences=True, return_state=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_output2, state_h2, state_c2 = encoder_lstm2(encoder_output1)
+encoder_lstm3 = LSTM(hidden_size, return_state=True, return_sequences=True, dropout=0.4, recurrent_dropout=0.4)
+encoder_outputs, state_h, state_c= encoder_lstm3(encoder_output2)
+
+decoder_inputs = Input(shape=(None,))
+dec_emb_layer = Embedding(tar_vocab, embedding_dim)
+dec_emb = dec_emb_layer(decoder_inputs)
+decoder_lstm = LSTM(hidden_size, return_sequences = True, return_state = True, dropout = 0.4, recurrent_dropout=0.2)
+decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state = [state_h, state_c])
+decoder_softmax_layer = Dense(tar_vocab, activation = 'softmax')
+decoder_softmax_outputs = decoder_softmax_layer(decoder_outputs)
+
+model = Model([encoder_inputs, decoder_inputs], decoder_softmax_outputs)
+model.summary()
+
+from attention import AttentionLayer
+
+attn_layer = AttentionLayer(name='attention_layer')
+attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+decoder_concat_input = Concatenate(axis = -1, name='concat_layer')([decoder_outputs, attn_out])
+
+decoder_softmax_layer = Dense(tar_vocab, activation='softmax')
+decoder_softmax_outputs = decoder_softmax_layer(decoder_concat_input)
+
+model = Model([encoder_inputs, decoder_inputs], decoder_softmax_outputs)
+model.summary()
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience = 2)
+history = model.fit(x = [encoder_input_train, decoder_input_train], y = decoder_target_train, \
+          validation_data = ([encoder_input_test, decoder_input_test], decoder_target_test),
+          batch_size = 256, callbacks=[es], epochs = 10)
+
+src_index_to_word = src_tokenizer.index_word # 원문 단어 집합에서 정수 -> 단어를 얻음
+tar_word_to_index = tar_tokenizer.word_index # 요약 단어 집합에서 단어 -> 정수를 얻음
+tar_index_to_word = tar_tokenizer.index_word # 요약 단어 집합에서 정수 -> 단어를 얻음
+
+encoder_model = Model(inputs=encoder_inputs, outputs=[encoder_outputs, state_h, state_c])
+decoder_state_input_h = Input(shape=(hidden_size,))
+decoder_state_input_c = Input(shape=(hidden_size,))
+
+dec_emb2 = dec_emb_layer(decoder_inputs)
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=[decoder_state_input_h, decoder_state_input_c])
+decoder_hidden_state_input = Input(shape=(text_max_len, hidden_size))
+attn_out_inf, attn_states_inf = attn_layer([decoder_hidden_state_input, decoder_outputs2])
+decoder_inf_concat = Concatenate(axis=-1, name='concat')([decoder_outputs2, attn_out_inf])
+
+decoder_outputs2 = decoder_softmax_layer(decoder_inf_concat)
+decoder_model = Model(
+    [decoder_inputs] + [decoder_hidden_state_input,decoder_state_input_h, decoder_state_input_c],
+    [decoder_outputs2] + [state_h2, state_c2])
+
+def decode_sequence(input_seq):
+ e_out, e_h, e_c = encoder_model.predict(input_seq)
+
+ target_seq = np.zeros((1, 1))
+ target_seq[0, 0] = tar_word_to_index['sostoken']
+
+ stop_condition = False
+ decoded_sentence = ''
+ while not stop_condition:
+  output_tokens, h, c = decoder_model.predict([target_seq] + [e_out, e_h, e_c])
+  sampled_token_index = np.argmax(output_tokens[0, -1, :])
+  sampled_token = tar_index_to_word[sampled_token_index]
+
+  if (sampled_token != 'eostoken'):
+   decoded_sentence += ' ' + sampled_token
+
+  if (sampled_token == 'eostoken' or len(decoded_sentence.split()) >= (summary_max_len - 1)):
+   stop_condition = True
+
+  target_seq = np.zeros((1, 1))
+  target_seq[0, 0] = sampled_token_index
+  e_h, e_c = h, c
+
+ return decoded_sentence
+
+def seq2text(input_seq):
+    sentence=''
+    for i in input_seq:
+        if(i!=0):
+            sentence = sentence + src_index_to_word[i]+' '
+    return sentence
+
+def seq2summary(input_seq):
+    sentence=''
+    for i in input_seq:
+        if((i!=0 and i!=tar_word_to_index['sostoken']) and i!=tar_word_to_index['eostoken']):
+            sentence = sentence + tar_index_to_word[i] + ' '
+    return sentence
+
+for i in range(500, 1000):
+    print("원문 : ",seq2text(encoder_input_test[i]))
+    print("실제 요약문 :",seq2summary(decoder_input_test[i]))
+    print("예측 요약문 :",decode_sequence(encoder_input_test[i].reshape(1, text_max_len)))
+    print("\n")
