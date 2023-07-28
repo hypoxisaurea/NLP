b6ab22c79f0164cb939757d2495531b52d816873
hypoxisaurea <flickerlight14@naver.com>
2023-07-28 21:27:39 +0900

    chatbot transformer

---
+++ chat_transformer.py
@@ -0,0 +1,134 @@
+import re
+import pandas as pd
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from transformer import transformer, CustomSchedule, loss_function
+
+#----------- 데이터 삽입 --------------
+train_data = pd.read_csv('content/ChatBotData.csv')
+print('전처리 전 챗봇 샘플의 개수 :', len(train_data))
+
+#----------- 전처리 --------------
+questions = []
+for sentence in train_data['Q']:
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = sentence.strip()
+    questions.append(sentence)
+
+answers = []
+for sentence in train_data['A']:
+    sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    sentence = sentence.strip()
+    answers.append(sentence)
+
+tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(questions + answers, target_vocab_size=2**13)
+START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+VOCAB_SIZE = tokenizer.vocab_size + 2
+
+#----------- 정수 인코딩 & 패딩 --------------
+sample_string = questions[20]
+tokenized_string = tokenizer.encode(sample_string)
+original_string = tokenizer.decode(tokenized_string)
+
+MAX_LENGTH = 40
+def tokenize_and_filter(inputs, outputs):
+  tokenized_inputs, tokenized_outputs = [], []
+
+  for (sentence1, sentence2) in zip(inputs, outputs):
+    sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+    sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+    tokenized_inputs.append(sentence1)
+    tokenized_outputs.append(sentence2)
+
+  tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(tokenized_inputs, maxlen=MAX_LENGTH, padding='post')
+  tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(tokenized_outputs, maxlen=MAX_LENGTH, padding='post')
+
+  return tokenized_inputs, tokenized_outputs
+
+questions, answers = tokenize_and_filter(questions, answers)
+
+#----------- 인코더 & 디코더 --------------
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+dataset = tf.data.Dataset.from_tensor_slices((
+    {
+        'inputs': questions,
+        'dec_inputs': answers[:, :-1]
+    },
+    {
+        'outputs': answers[:, 1:]
+    },
+))
+
+dataset = dataset.cache()
+dataset = dataset.shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE)
+dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+#----------- 트랜스포머 --------------
+tf.keras.backend.clear_session()
+
+D_MODEL = 256
+NUM_LAYERS = 2
+NUM_HEADS = 8
+DFF = 512
+DROPOUT = 0.1
+
+model = transformer(
+    vocab_size=VOCAB_SIZE,
+    num_layers=NUM_LAYERS,
+    dff=DFF,
+    d_model=D_MODEL,
+    num_heads=NUM_HEADS,
+    dropout=DROPOUT)
+
+learning_rate = CustomSchedule(D_MODEL)
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+def accuracy(y_true, y_pred):
+  y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+  return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+
+model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
+
+EPOCHS = 10
+model.fit(dataset, epochs=EPOCHS)
+
+#----------- 챗봇 --------------
+def preprocess_sentence(sentence):
+  sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+  sentence = sentence.strip()
+  return sentence
+
+def evaluate(sentence):
+  sentence = preprocess_sentence(sentence)
+  sentence = tf.expand_dims(START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+  output = tf.expand_dims(START_TOKEN, 0)
+
+  for i in range(MAX_LENGTH):
+    predictions = model(inputs=[sentence, output], training=False)
+    predictions = predictions[:, -1:, :]
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+    if tf.equal(predicted_id, END_TOKEN[0]):
+      break
+    output = tf.concat([output, predicted_id], axis=-1)
+
+  return tf.squeeze(output, axis=0)
+
+def predict(sentence):
+  prediction = evaluate(sentence)
+  predicted_sentence = tokenizer.decode([i for i in prediction if i < tokenizer.vocab_size])
+
+  print('Input: {}'.format(sentence))
+  print('Output: {}'.format(predicted_sentence))
+
+  return predicted_sentence
+
+
+output = predict("영화 볼래?")
+output = predict("고민이 있어")
+output = predict("너무 화가나")
+output = predict("카페갈래?")
+output = predict("게임하고싶당")
